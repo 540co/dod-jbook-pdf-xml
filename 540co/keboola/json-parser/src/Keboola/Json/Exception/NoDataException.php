55c9db7f7a44de08c8f48ec9615534ec8938a039
John OBrien <jobrieniii@yahoo.com>
2019-10-13 08:22:43 -0400

    cleanup

--- 540co/keboola/json-parser/src/Keboola/Json/Exception/NoDataException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Keboola\Json\Exception;
-
-class NoDataException extends JsonParserException
-{
-}
--- 540co/keboola/php-utils/src/Keboola/Utils/Exception/NoDataFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Keboola\Utils\Exception;
-
-use Keboola\Utils\Exception;
-
-class NoDataFoundException extends Exception
-{
-
-}
